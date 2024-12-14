222781997554d5331bf9dcb2552239a35d9cbd25
Amos Ndonga <amosndonga@gmail.com>
2024-12-14 20:53:59 +0300

    Added README.md

--- Questions/AnagramsTest.cs
+++ Questions/AnagramsTest.cs
@@ -4,11 +4,62 @@ public static class CheckIfTwoStringsAreAnagrams
 {
     /*
     Two strings are anagrams if they have same length, and same characters with same frequend
+
+    First check if the two strings have same length
+
+    Create two dictionary ,key to be the characters and value frequency
+
+    compare the two dictionaries
     
     */
 
     public static bool Check(string String1, string String2)
     {
+        //Check if have same length
+        if(String1.Length != String2.Length)
+        {
+            return false;
+        }
+
+        //Create two dictionaries
+        // Create a Dictionary
+        Dictionary<char, int> dict1 = [];
+        Dictionary<char, int> dict2 = [];
+
+        //Loop through each string
+        foreach(var Item in String1)
+        {
+            if(dict1.ContainsKey(Item))
+            {
+                dict1[Item] += 1;
+            }
+            else
+            {
+                dict1[Item] = 1;
+            }
+        }
+
+        foreach(var Item in String2)
+        {
+            if(dict2.ContainsKey(Item))
+            {
+                dict2[Item] += 1;
+            }
+            else
+            {
+                dict2[Item] = 1;
+            }
+        }
+
+        foreach (char key in dict1.Keys)
+        {
+            if(!dict2.ContainsKey(key) || dict1[key] != dict2[key])
+            {
+                return false;
+            }
+        }
+
+
         return true;
     }
 
---
+++ README.md
@@ -0,0 +1,8 @@
+## 10 Most Coding Interview Questions (Solved using C# and VS Code)
+
+### 1. Checking if two strings are anagrams
+Two strings are anagram if they have same length, and each character occurs in same frequency,
+e.g, 
+``
+danger and garden
+``
