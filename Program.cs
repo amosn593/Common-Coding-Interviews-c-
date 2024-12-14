0ecc0ca56eedb3363df678c414901d5ffe8e05d5
Amos Ndonga <amosndonga@gmail.com>
2024-12-14 20:10:06 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using InterviewQuestion.Question;
+
+Console.WriteLine("Hello, World!");
+
+// 1. Check if two strings are anagram
+var result = CheckIfTwoStringsAreAnagrams.Check("t", "t");
+
+Console.WriteLine(result);
+
+Console.ReadLine();
---
+++ Questions/AnagramsTest.cs
@@ -0,0 +1,15 @@
+namespace InterviewQuestion.Question;
+
+public static class CheckIfTwoStringsAreAnagrams
+{
+    /*
+    Two strings are anagrams if they have same length, and same characters with same frequend
+    
+    */
+
+    public static bool Check(string String1, string String2)
+    {
+        return true;
+    }
+
+}
