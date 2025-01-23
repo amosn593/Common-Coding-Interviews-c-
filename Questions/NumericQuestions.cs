7b94664c9a3f45455ed6db5017158afcf83c4df9
Amos Ndonga <amosndonga@gmail.com>
2025-01-23 22:47:32 +0300

    vb

--- Questions/NumericQuestions.cs
+++ Questions/NumericQuestions.cs
@@ -13,24 +13,31 @@ public class NumericQuestions
 
     public static void TestPrimeNumber(int testNumber)
     {
-        if(IsPrimeNumber(testNumber)){
+        bool isPrime = IsPrime(testNumber);
+        if(isPrime)
+        {
             Console.WriteLine($"{testNumber} is PrimeNumber");
         }
-        else{
+        else
+        {
             Console.WriteLine($"{testNumber} is Not PrimeNumber");
         }
     }
 
-    private static bool IsPrimeNumber(int testNumber){
-        for(int i = 2; i <= testNumber/2; i++){
-            if(testNumber%i==0){
-                return false;
-            }
-            else{
-                return true;
-            }
+    static bool IsPrime(int number)
+    {
+        if (number <= 1) return false;
+        if (number == 2) return true;
+        if (number % 2 == 0) return false;
+
+        int boundary = (int)Math.Floor(Math.Sqrt(number));
+
+        for (int i = 3; i <= boundary; i += 2)
+        {
+            if (number % i == 0) return false;
         }
-        return false;
+
+        return true;
     }
 
 
